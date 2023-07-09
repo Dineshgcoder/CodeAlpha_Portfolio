863f52704655362cf6656bd7b6602361bf39a9ab
Dinesh Gupta <118104005+Dineshgcoder@users.noreply.github.com>
2023-07-09 14:56:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+// let cursor1 = document.querySelector('.cursor-1');
+// let cursor2 = document.querySelector('.cursor-2');
+
+// window.onmousemove = (e) =>{
+//     cursor1.style.top = e.pageY + 'px';
+//     cursor1.style.left = e.pageX + 'px';
+//     cursor2.style.top = e.pageY + 'px';
+//     cursor2.style.left = e.pageX + 'px';
+// }
+
+// document.querySelectorAll('a').forEach(links =>{
+
+//     links.onmouseenter = () =>{
+//         cursor1.classList.add('active');
+//         cursor2.classList.add('active');
+//     }
+
+//     links.onmouseleave = () =>{
+//         cursor1.classList.remove('active');
+//         cursor2.classList.remove('active');
+//     }
+
+// });
+
+src="https://smtpjs.com/v3/smtp.js";
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
